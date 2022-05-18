5bedf0be162d13596d37d48bc515218484e02afb
davidoff-a <davidoff.aleksei@gmail.com>
2022-05-18 10:32:10 +0300

    refactor: use stdout instead console.log

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,12 +2,11 @@ const fs = require("fs");
 const path = require("path");
 
 try {
+  const { stdout } = process;
   const readStream = fs.createReadStream(path.join(__dirname, "text.txt"), {
     encoding: "utf-8",
   });
-  readStream.on("data", (data) => {
-    console.log(data);
-  });
+  readStream.on("data", (data) => stdout.write(data));
 } catch (error) {
   throw new Error(`Error of ${error} was taken`);
 }
