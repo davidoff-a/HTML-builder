f690802cdb223317da028b7f4f87efed10733f76
davidoff-a <davidoff.aleksei@gmail.com>
2022-05-20 09:51:09 +0300

    feat: add 03-files-in-folder solution

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,29 +1,29 @@
-const { readdir } = require("fs/promises");
+const { readdir, stat } = require("fs/promises");
 const path = require("path");
-const { stdout } = process;
+// const { stdout } = process;
 
 try {
-  await readdir(path.resolve(__dirname, "secret-folder"));
+  readdir(path.resolve(__dirname, "secret-folder"), {
+    withFileTypes: true,
+  }).then((data) => {
+    console.log(data);
+    for (let fileName of data) {
+      try {
+        stat(path.resolve(__dirname, "secret-folder", fileName.name)).then(
+          (fileData) => {
+            if (fileName && !fileName.isDirectory()) {
+              const nameParts = fileName.name.split(".");
+              console.table(
+                `${nameParts[0]} - ${nameParts[1]} - ${fileData.size / 1024} Kb`
+              );
+            }
+          }
+        );
+      } catch (error) {
+        console.error("there was an error:", error.message);
+      }
+    }
+  });
 } catch (error) {
   console.error("there was an error:", error.message);
 }
-const readDir = await fs
-  .readdir(path.resolve(__dirname, "secret-folder"), {
-    withFileTypes: true,
-  })
-  .then((files) => {
-    for (let file of files) {
-      const nameParts = file.name.split(".");
-      fs.stat(
-        path.resolve(__dirname, "secret-folder", file.name),
-        (err, stats) => {
-          if (err) {
-            throw new Error(err.message);
-          }
-          console.log(stats);
-          console.log(stats.isDirectory);
-        }
-      );
-      stdout.write(`${nameParts[0]} - ${nameParts[1]}\n`);
-    }
-  });
