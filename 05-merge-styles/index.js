e106bd51a23aaa1ddc9969b2ffe091be9ea342dd
davidoff-a <davidoff.aleksei@gmail.com>
2022-05-20 15:50:26 +0300

    feat: add 05-merge-styles solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,32 @@
+const { readdir, stat } = require("fs/promises");
+const path = require("path");
+const fs = require("fs");
+// const { stdout } = process;
+const writeStream = fs.createWriteStream(
+  path.resolve(__dirname, "project-dist", "bundle.css"),
+  { encoding: "utf-8" }
+);
+try {
+  readdir(path.resolve(__dirname, "styles"), {
+    withFileTypes: true,
+  }).then((data) => {
+    for (let fileName of data) {
+      if (!fileName.isDirectory() && fileName.name.slice(-3) === "css") {
+        const readStream = fs.createReadStream(
+          path.join(__dirname, "styles", fileName.name),
+          {
+            encoding: "utf-8",
+          }
+        );
+        readStream.on("data", (styles) => {
+          writeStream.write(styles);
+        });
+        readStream.on("end", () => {
+          readStream.close();
+        });
+      }
+    }
+  });
+} catch (error) {
+  console.error("there was an error:", error.message);
+}
